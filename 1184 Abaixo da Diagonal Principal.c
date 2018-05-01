b98e6f23441b9640e78c38e63af463d8c5027119
marcelolima381 <marcelolima381@gmail.com>
2018-05-01 00:10:00 -0300

    Terminei todos os exercícios de diagonal em matrizes

---
+++ 1184 Abaixo da Diagonal Principal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+  float matriz[12][12], val = 0, count = 0;
+  int i, j;
+  char operacao;
+
+  scanf("%c", &operacao);
+
+  for(i = 0; i < 12; i++) {
+    for(j = 0; j < 12; j++) {
+      scanf("%f", &matriz[i][j]);
+      if (j < i) {
+        val += matriz[i][j];
+        count++;
+      }
+    }
+  }
+
+  if (operacao == 'M') {
+    val /= (float)count;
+  }
+
+  printf("%.1f\n", val);
+}
---
+++ 1190 Área Direita.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+  double matriz[12][12], val = 0, count = 0;
+  int i, j;
+  char operacao;
+  // srand(time(NULL));
+  scanf("%c", &operacao);
+
+  for(i = 0; i < 12; i++) {
+    for(j = 0; j < 12; j++) {
+      // matriz[i][j]=rand() % 100;
+      scanf("%lf", &matriz[i][j]);
+      if (i + j >= 12 && j > i) {
+        val += matriz[i][j];
+        count++;
+      }
+    }
+  }
+
+  if (operacao == 'M') {
+    val /= (double)count;
+  }
+
+  printf("%.1lf\n", val);
+}
